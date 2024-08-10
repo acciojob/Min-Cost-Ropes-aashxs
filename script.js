e3fb9b72b4448a94ba18d4ce3ccbae5d8682afb7
Ashish <90548985+aashxs@users.noreply.github.com>
2024-08-10 08:49:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	const brr = [];
+arr.sort((a, b) => {
+  a - b;
+});
+
+arr.reduce((value, item) => {
+  value = value + item;
+
+  brr.push(value);
+  return value;
+});
+   let a=0;
+  brr.forEach((value)=>{
+    a = a+value;
+      
+
+  })
+  console.log(a);
   
 }
 
